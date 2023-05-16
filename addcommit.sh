6c0c2ee89b4a1474e14b8c97c5e6fdbba04a488d
hamster1963 <1410514192@qq.com>
2023-05-17 02:05:33 +0800

    fix: 测试构建打版本号

--- addcommit.sh
+++ addcommit.sh
@@ -1,7 +1,7 @@
 # 获取git tag版本号
 GitTag=$(git tag --sort=version:refname | tail -n 1)
 # 获取源码最近一次 git commit log，包含 commit sha 值，以及 commit message
-GitCommitLog=$(git log --pretty=oneline -n 1)
+GitCommitLog=$(git log --pretty=oneline -1 --no-merges)
 # 获取当前时间
 BuildTime=$(date +'%Y.%m.%d.%H:%M:%S')
 # 获取Go的版本
@@ -13,12 +13,12 @@ echo "GitCommitLog: ${GitCommitLog}"
 echo "BuildTime: ${BuildTime}"
 echo "BuildGoVersion: ${BuildGoVersion}"
 
-# 将以上变量序列化至 LDFlags 变量中
-LDFlags=" \
-    -X 'push/utility/bin_utils.GitTag=${GitTag}' \
-    -X 'push/utility/bin_utils.GitCommitLog=${GitCommitLog}' \
-    -X 'push/utility/bin_utils.BuildTime=${BuildTime}' \
-    -X 'push/utility/bin_utils.BuildGoVersion=${BuildGoVersion}' \
-"
-
-go build -ldflags "$LDFlags" -o service
+# # 将以上变量序列化至 LDFlags 变量中
+# LDFlags=" \
+#     -X 'push/utility/bin_utils.GitTag=${GitTag}' \
+#     -X 'push/utility/bin_utils.GitCommitLog=${GitCommitLog}' \
+#     -X 'push/utility/bin_utils.BuildTime=${BuildTime}' \
+#     -X 'push/utility/bin_utils.BuildGoVersion=${BuildGoVersion}' \
+# "
+#
+# go build -ldflags "$LDFlags" -o service
